42034c324f618d69c2026aff211d4de23bff145b
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-09-03 22:01:34 -0300

    config, app, index, navbar

---
+++ src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render(){
+        return (
+            <div>
+                <h1>TODO</h1>
+            </div>
+        )
+    }
+
+}
